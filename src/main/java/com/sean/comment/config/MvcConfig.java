3f6fbcefd1f321043e80e4d1559cb91438d6c676
Sean <1224521413@qq.com>
2022-10-13 12:00:02 +0800

    基于 Redis 的短信验证登录
    该功能已完成

--- src/main/java/com/sean/comment/config/MvcConfig.java
+++ src/main/java/com/sean/comment/config/MvcConfig.java
@@ -1,15 +1,20 @@
 package com.sean.comment.config;
 
 import com.sean.comment.controller.interceptor.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@SuppressWarnings("all")
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginInterceptor())
+        registry.addInterceptor(new LoginInterceptor(stringRedisTemplate))
                 .excludePathPatterns(
                         "/user/code",
                         "/user/login",
--- src/main/java/com/sean/comment/controller/interceptor/LoginInterceptor.java
+++ src/main/java/com/sean/comment/controller/interceptor/LoginInterceptor.java
@@ -5,30 +5,57 @@ import cn.hutool.core.bean.BeanUtil;
 import com.sean.comment.dto.UserDTO;
 import com.sean.comment.entity.User;
 import com.sean.comment.utils.UserHolder;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static com.sean.comment.utils.RedisConstants.LOGIN_USER_KEY;
+import static com.sean.comment.utils.RedisConstants.LOGIN_USER_TTL;
 import static com.sean.comment.utils.SystemConstants.LOGIN_USER;
 
 public class LoginInterceptor implements HandlerInterceptor {
+    private StringRedisTemplate stringRedisTemplate;
+
+    public LoginInterceptor(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         // 获取 session
-        final HttpSession session = request.getSession();
+        // final HttpSession session = request.getSession();
         // 从 session 中获取用户
-        final User user = (User)session.getAttribute(LOGIN_USER);
-        // 判断用户是否存在
-        if(user == null){
+        // final User user = (User)session.getAttribute(LOGIN_USER);
+
+        // 获取请求头中的 token
+        final String token = request.getHeader("authorization");
+        if(StringUtils.isBlank(token)){
             response.setStatus(401); // 未授权
             return false;
         }
+        // 查询 redis 中的用户
+        final Map<Object, Object> userMap = stringRedisTemplate.opsForHash().entries(LOGIN_USER_KEY + token);
+        // 判断用户是否存在
+        if(userMap.isEmpty()){
+            response.setStatus(401); // 未授权
+            return false;
+        }
+        // 解析用户
+        final UserDTO userDTO = BeanUtil.fillBeanWithMap(userMap, new UserDTO(), false);
         // 保存用户到 ThreadLocal 中
 //        UserDTO userDTO = new UserDTO();
 //        userDTO.setId(user.getId());
 //        userDTO.setNickName(user.getNickName());
 //        userDTO.setIcon(user.getIcon());
-        UserHolder.saveUser(BeanUtil.copyProperties(user, UserDTO.class));
+        UserHolder.saveUser(userDTO);
+        // 刷新 token 有效期
+        stringRedisTemplate.expire(LOGIN_USER_KEY + token, LOGIN_USER_TTL, TimeUnit.MINUTES);
         // 放行
         return true;
     }
